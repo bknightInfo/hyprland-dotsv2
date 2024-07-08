416d614b03f7ab24c62e95ade9668cd8ab43bacb
Your Name <you@example.com>
2024-07-08 23:12:05 +1000

    remove scripts

--- scripts/laravel-fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $SCRIPT_DIR/storage 
-mkdir -pv framework/views app framework/sessions framework/cache 
-cd .. 
-chmod -R 777 storage
