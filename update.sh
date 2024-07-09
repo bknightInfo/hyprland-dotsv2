c5710fb288568be5fbfc962bd93350ee34aed310
Your Name <you@example.com>
2024-07-09 15:53:36 +1000

    linter fixes

--- update.sh
+++ update.sh
@@ -204,19 +204,19 @@ cp -f hook.js ~/dotfiles-version/
 
 #configure bat theme
 mkdir -p "$(bat --config-dir)/themes"
-cd "$(bat --config-dir)/themes"
+cd "$(bat --config-dir)/themes" || exit
 # Replace _night in the lines below with _day, _moon, or _storm if needed.
 curl -O https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_night.tmTheme
 bat cache --build
 bat --list-themes | grep tokyo # should output "tokyonight_night"
-echo '--theme="tokyonight_night"' >> "$(cat --config-dir)/config"
+echo '--theme="tokyonight_night"' >> "$(bat --config-dir)/config"
 
 #Display manager
 sudo pacman -S --noconfirm sddm
 sudo systemctl enable sddm.service
 
 # Cleaning orphan files
-sudo pacman -Rns --noconfirm $(pacman -Qtdq)
+sudo pacman -Rns --noconfirm "$(pacman -Qtdq)"
 
 echo "     _                   "
 echo "  __| | ___  _ __   ___  "
